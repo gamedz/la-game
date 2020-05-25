b65f9bf145648229bb0b137fbd99d0d2b428f75c
ignat <mr.glushikhin@gmail.com>
2020-05-25 09:12:22 +0300

    fixes scale #2

--- assets/intro.js
+++ assets/intro.js
@@ -106,10 +106,10 @@ const config = {
     parent: 'phaser-example',
     width: 1024,
     height: 768,
-     scale: {
+     // scale: {
     //     mode: Phaser.Scale.FIT,
-         autoCenter: Phaser.Scale.CENTER_BOTH         
-     },    
+     //     autoCenter: Phaser.Scale.CENTER_BOTH         
+     // },    
     dom: {
         createContainer: true
     },
